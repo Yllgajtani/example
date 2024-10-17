24483e16c06c1dd06cd3d5ee18ce82591cb5ba1f
Yll Gajtani <123652630+Yllgajtani@users.noreply.github.com>
2024-10-17 18:57:46 +0200

    Add files via upload

---
+++ example2/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+});
+
+
+app.post("/", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log("Username: " + username);
+    console.log("Password: " + password);
+    res.send("Data received");
+});
+
+
+app.listen(2030);
